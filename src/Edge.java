a26c9c26a27d93de1f878e72f4b9e00c4b21b61b
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-05-02 16:06:23 -0400

    Add instance fields and constructor

--- src/Edge.java
+++ src/Edge.java
@@ -6,5 +6,21 @@
  */
 
 public class Edge {
-    
+
+    private Vertex v; // The other vertex of the edge
+    private Vertex u; // The vertex of the edge
+    private int distance; // The length of the edge
+
+    /**
+     * Constructor to initialize an edge with two vertices and a distance.
+     * 
+     * @param u the vertex of the edge
+     * @param v the other vertex of the edge
+     * @param distance the distance between the two vertices
+     */
+    public Edge(Vertex u, Vertex v, double distance){
+        this.u = u;
+        this.v = v;
+        this.distance = (int) distance;
+    }
 }
